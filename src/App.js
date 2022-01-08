03578d9c7c37a1363e3e5ef6fc0bee5550d7d36c
Josh Gordon <github@joshgordon.net>
2022-01-07 20:53:34 -0500

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+// import 'semantic-ui-css/semantic.min.css'
+import {useState, useEffect} from 'react'
+
+import {Input} from "semantic-ui-react"
+import WordList from './Wordlist';
+
+function update(callback) {
+  function onChange(event) {
+    callback(event.target.value)
+  }
+  return onChange
+}
 
 function App() {
+  let [regex, setRegex] = useState("")
+  let [must, setMust] = useState("")
+  let [cant, setCant] = useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Input placeholder="Regex" value={regex} onChange={update(setRegex)} type="text"></Input>
+        <Input placeholder="Must include" value={must} onChange={update(setMust)} type="text"></Input>
+        <Input placeholder="Can't include" value={cant} onChange={update(setCant)} type="text"></Input>
+        <WordList regex={regex} must={must} cant={cant}/>
     </div>
+
+
   );
 }
 
---
+++ src/Wordlist.js
@@ -0,0 +1,62 @@
+import {useState, useEffect} from 'react'
+import {Message, List, ListItem} from 'semantic-ui-react'
+
+function WordList(props) {
+  let [words, setWords] = useState([])
+  useEffect(() => {
+    fetch("/5words.txt").then((res) => res.text()).then((wordlist) => setWords(wordlist.split("\n")))
+  }, [])
+
+  let [re, setre] = useState(null)
+
+  let must = props.must
+  let cant = props.cant
+  let regex = props.regex
+
+  useEffect(() => {
+      //try {new RegExp("fo[o") } catch (e) {alert(e)} 
+
+
+    try {
+        setre(new RegExp(regex))
+    }
+    catch (e) {
+        setre(null)
+    }
+  }, [props.regex])
+
+  if (re === null) {
+        return (<Message warning>
+            <Message.Header>Invalid Regex</Message.Header>
+            <p>Please try again</p>
+        </Message>)
+  }
+    return (
+        <List>
+          {words.filter((word) => {
+            if (re.test(word)) {
+              // go through the musts:
+              for (let ii=0; ii < must.length; ii++) {
+                if (word.indexOf(must[ii]) < 0) {
+                  return false
+                }
+              }
+
+              //and the cant's:
+              for (let ii=0; ii < cant.length; ii++) {
+                if (word.indexOf(cant[ii]) >= 0) {
+                  return false
+                }
+              }
+              return true
+            }
+          return false
+          }).map(word => (
+            <ListItem>{word}</ListItem>
+          ))}
+
+        </List>
+    )
+}
+
+export default WordList
